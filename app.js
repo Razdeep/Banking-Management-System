b556f8333304604667cafbb033b0d02e8359db91
razdeep <rrajdeeproychowdhury@gmail.com>
2018-03-14 23:33:25 +0530

    Node structure Improved

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 var express=require('express');
+var path=require('path');
 var app=express();
 var port=1998;
+app.use(express.static(path.join(__dirname, 'public')));
 app.get('/', function(req,res)
 {
-    res.sendFile(__dirname+'/ui/register.html');
+    res.sendFile(__dirname+'/ui/index.html');
 });
 app.listen(port,function(){
     console.log("Server started at port "+port);
