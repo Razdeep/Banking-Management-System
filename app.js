1d6ece71a9ee66a19faf01429518bd0ec870d0fb
razdeep <rrajdeeproychowdhury@gmail.com>
2018-03-15 18:44:47 +0530

    Support for register page from localhost added

--- app.js
+++ app.js
@@ -7,6 +7,10 @@ app.get('/', function(req,res)
 {
     res.sendFile(__dirname+'/ui/index.html');
 });
+app.get('/register',function(req,res)
+{
+    res.sendFile(__dirname+'/ui/register.html');
+});
 app.listen(port,function(){
     console.log("Server started at port "+port);
     console.log("http://localhost:1998");
