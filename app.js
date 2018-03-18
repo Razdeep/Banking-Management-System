19731310d91143a2040043314293764006df2c8d
razdeep <rrajdeeproychowdhury@gmail.com>
2018-03-18 13:29:53 +0530

    Morgan Logger added to project

--- app.js
+++ app.js
@@ -1,8 +1,14 @@
 var express=require('express');
 var path=require('path');
+var logger=require('morgan');
+
+
 var app=express();
 var port=1998;
 app.use(express.static(path.join(__dirname, 'public')));
+app.set('view engine','ejs');
+app.use(logger('dev'));
+
 app.get('/', function(req,res)
 {
     res.sendFile(__dirname+'/ui/index.html');
@@ -11,6 +17,7 @@ app.get('/register',function(req,res)
 {
     res.sendFile(__dirname+'/ui/register.html');
 });
+
 app.listen(port,function(){
     console.log("Server started at port "+port);
     console.log("http://localhost:1998");
